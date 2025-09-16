1d5935d7a8ea06d74aad43fbc16ca2eabbced99c
Ayush Arora <ayusharora10hg@gmail.com>
2025-09-15 21:33:00 -0700

    UI update

--- Ayush_Arora_MyRuns1/app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns1/UserProfileActivity.kt
+++ Ayush_Arora_MyRuns1/app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns1/UserProfileActivity.kt
@@ -3,13 +3,11 @@ package ca.sfu.cmpt362.ayusharora.myruns1
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class UserProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.user_profile_activity)
     }
 }
