6ef264ebf7fb3a77f9fb3aec5a33d2b1b498317a
Ayush Arora <ayusharora10hg@gmail.com>
2025-09-21 15:11:32 -0700

    completed webpage preference

--- app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns/SettingsFragment.kt
+++ app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns/SettingsFragment.kt
@@ -4,14 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.core.net.toUri
 
 class SettingsFragment : PreferenceFragmentCompat() {
-    override fun onCreatePreferences( savedInstanceState: Bundle?, rootKey: String?) {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         this.addPreferencesFromResource(R.xml.preference)
 
         val userProfilePreference = findPreference<Preference>("user_profile_preference")
         userProfilePreference?.setOnPreferenceClickListener {
             val intent = Intent(requireContext(), UserProfileActivity::class.java)
+            startActivity(intent)
+            true
+        }
+
+        val webpagePreference = findPreference<Preference>("webpage_preference")
+        webpagePreference?.setOnPreferenceClickListener {
+            val url = getString(R.string.webpageURL)
+            val intent = Intent(Intent.ACTION_VIEW, url.toUri())
             startActivity(intent)
             true
         }
