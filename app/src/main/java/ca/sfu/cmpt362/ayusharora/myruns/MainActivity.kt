636777e48904fab6815d1b5d54a8564b8faf4110
Ayush Arora <ayusharora10hg@gmail.com>
2025-09-21 01:01:28 -0700

    added fragment navigation

--- app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns/MainActivity.kt
+++ app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns/MainActivity.kt
@@ -1,13 +1,37 @@
 package ca.sfu.cmpt362.ayusharora.myruns
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
 
+private lateinit var mainTabLayout : TabLayout
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainFragmentContainer, StartFragment())
+            .commit()
+
+        mainTabLayout = findViewById(R.id.mainTabLayout)
+
+        mainTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                val fragment = when (tab.position) {
+                    0 -> StartFragment()
+                    1 -> HistoryFragment()
+                    2 -> SettingsFragment()
+                    else -> StartFragment()
+                }
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.mainFragmentContainer, fragment)
+                    .commit()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) = Unit
+            override fun onTabReselected(tab: TabLayout.Tab?) = Unit
+        })
     }
 }
