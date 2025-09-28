e09c4371012bd1fec5ecfd98faec93511aa4690d
Ayush Arora <ayusharora10hg@gmail.com>
2025-09-27 17:06:21 -0700

    added items to spinner widgets using adapters

--- app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns/StartFragment.kt
+++ app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns/StartFragment.kt
@@ -4,14 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 
 class StartFragment : Fragment() {
+
+    private lateinit var inputTypeSpinner: Spinner
+    private lateinit var activityTypeSpinner: Spinner
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        val ret =  inflater.inflate(R.layout.fragment_start, container, false)
+
+        inputTypeSpinner = ret.findViewById<Spinner>(R.id.start_spinner_input)
+        activityTypeSpinner = ret.findViewById<Spinner>(R.id.start_spinner_activity)
+
+        val inputAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_item,
+            resources.getStringArray(R.array.input_type)
+        )
+        inputAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        inputTypeSpinner.adapter = inputAdapter
+
+        val activityAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_item,
+            resources.getStringArray(R.array.activity_type)
+        )
+        activityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        activityTypeSpinner.adapter = activityAdapter
+
+        return ret
     }
 }
