2a229131f8fec85813a4e85133a0b4a611ffa176
Ayush Arora <ayusharora10hg@gmail.com>
2025-09-27 21:18:23 -0700

    end of day

--- app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns/fragments/DialogFragment.kt
+++ app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns/fragments/DialogFragment.kt
@@ -4,15 +4,14 @@ import android.content.DialogInterface
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 
-class MyRunsDialogFragment: DialogFragment(), DialogInterface.OnClickListener {
-
+class DialogFragment : DialogFragment(), DialogInterface.OnClickListener {
 
 
     override fun onClick(dialog: DialogInterface?, item: Int) {
         if (item == DialogInterface.BUTTON_POSITIVE) {
-            Toast.makeText(activity, "Saved!", Toast.LENGTH_LONG).show()
+            Toast.makeText(activity, "ok clicked", Toast.LENGTH_LONG).show()
         } else if (item == DialogInterface.BUTTON_NEGATIVE) {
-            Toast.makeText(activity, "Discarded!", Toast.LENGTH_LONG).show()
+            Toast.makeText(activity, "cancel clicked", Toast.LENGTH_LONG).show()
         }
     }
 }
