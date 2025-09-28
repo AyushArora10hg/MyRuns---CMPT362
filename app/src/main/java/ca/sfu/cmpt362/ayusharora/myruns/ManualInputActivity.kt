25269fc4866417f75b4c5bd46b9471411752f96c
Ayush Arora <ayusharora10hg@gmail.com>
2025-09-27 17:19:56 -0700

    implemented start activity start button

---
+++ app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns/ManualInputActivity.kt
@@ -0,0 +1,12 @@
+package ca.sfu.cmpt362.ayusharora.myruns
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ManualInputActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manual_input)
+    }
+}
--- app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns/StartFragment.kt
+++ app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns/StartFragment.kt
@@ -1,10 +1,12 @@
 package ca.sfu.cmpt362.ayusharora.myruns
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
 
@@ -12,6 +14,7 @@ class StartFragment : Fragment() {
 
     private lateinit var inputTypeSpinner: Spinner
     private lateinit var activityTypeSpinner: Spinner
+    private lateinit var startButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,8 +22,9 @@ class StartFragment : Fragment() {
     ): View? {
         val ret =  inflater.inflate(R.layout.fragment_start, container, false)
 
-        inputTypeSpinner = ret.findViewById<Spinner>(R.id.start_spinner_input)
-        activityTypeSpinner = ret.findViewById<Spinner>(R.id.start_spinner_activity)
+        inputTypeSpinner = ret.findViewById(R.id.start_spinner_input)
+        activityTypeSpinner = ret.findViewById(R.id.start_spinner_activity)
+        startButton = ret.findViewById(R.id.start_button_start)
 
         val inputAdapter = ArrayAdapter(
             requireContext(),
@@ -37,6 +41,19 @@ class StartFragment : Fragment() {
         )
         activityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         activityTypeSpinner.adapter = activityAdapter
+
+        startButton.setOnClickListener {
+            when(inputTypeSpinner.selectedItemPosition){
+                0->{
+                    val intent = Intent(requireContext(), ManualInputActivity::class.java)
+                    startActivity(intent)
+                }
+                1, 2->{
+                    val intent = Intent(requireContext(), MapDisplayActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
 
         return ret
     }
