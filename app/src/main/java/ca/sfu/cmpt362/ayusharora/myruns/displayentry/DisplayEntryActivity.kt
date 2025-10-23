8dcb7050caf4021b8b6cfad04a61df42617abcb7
Ayush Arora <ayusharora10hg@gmail.com>
2025-10-23 02:27:29 -0700

    basic UI of display entry activity

---
+++ app/src/main/java/ca/sfu/cmpt362/ayusharora/myruns/displayentry/DisplayEntryActivity.kt
@@ -0,0 +1,13 @@
+package ca.sfu.cmpt362.ayusharora.myruns.displayentry
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ca.sfu.cmpt362.ayusharora.myruns.R
+
+class DisplayEntryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.setContentView(R.layout.activity_display_entry)
+    }
+}
